f11ddc10cce015d7f3ba81e660b4125f40cabeb3
eylulstudio <eylulbasakdonmez5@gmail.com>
2025-12-09 12:37:22 +0300

    Initialize project with basic structure and core classes

---
+++ src/com/bank/Account.java
@@ -0,0 +1,32 @@
+package com.bank;
+
+public class Account {
+    protected String accountNumber;
+    protected double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+}
