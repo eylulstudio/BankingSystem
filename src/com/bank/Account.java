00938d3fc946f69f1efc518deaa9cef5b9b4787e
eylulstudio <eylulbasakdonmez5@gmail.com>
2025-12-19 23:26:45 +0300

    Add transaction history and record deposit transactions

--- src/com/bank/Account.java
+++ src/com/bank/Account.java
@@ -1,23 +1,29 @@
 package com.bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Account implements Transferable {
 
     protected String accountNumber;
     private double balance;
+    protected List<Transaction> transactions;
 
     public Account(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.transactions = new ArrayList<>();
     }
 
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
+            transactions.add(new Transaction("Deposit", amount));
         }
     }
 
     public abstract boolean withdraw(double amount);
-    
+
     @Override
     public void transfer(Account target, double amount) {
         if (withdraw(amount)) {
@@ -25,13 +31,18 @@ public abstract class Account implements Transferable {
         }
     }
 
+    public void printTransactions() {
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+    }
 
     public double getBalance() {
         return balance;
     }
-    
+
     protected void setBalance(double balance) {
-    	this.balance=balance;
+        this.balance = balance;
     }
 
     public String getAccountNumber() {
