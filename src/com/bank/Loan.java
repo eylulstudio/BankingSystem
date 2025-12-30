d8d2c0b991a2194d80016266238a934a544ee4fa
eylulstudio <eylulbasakdonmez5@gmail.com>
2025-12-30 16:55:35 +0300

    Finalize Loan class with repayment and fixed interest

--- src/com/bank/Loan.java
+++ src/com/bank/Loan.java
@@ -2,28 +2,62 @@ package com.bank;
 
 public class Loan {
 
-    private double principal;
-    private double interestRate;
-    private double remainingDebt;
+    private String loanId;
+    private double principal;          
+    private double totalDebt;          
+    private double remainingDebt;      
 
-    public Loan(double principal, double interestRate) {
+    private static final double INTEREST_RATE = 0.10; 
+
+    public Loan(String loanId, double principal) {
+        this.loanId = loanId;
         this.principal = principal;
-        this.interestRate = interestRate;
-        this.remainingDebt = principal + (principal * interestRate);
+        this.totalDebt = principal + (principal * INTEREST_RATE);
+        this.remainingDebt = this.totalDebt;
+    }
+
+    public String getLoanId() {
+        return loanId;
+    }
+
+    public double getPrincipal() {
+        return principal;
+    }
+
+    public double getTotalDebt() {
+        return totalDebt;
     }
 
     public double getRemainingDebt() {
         return remainingDebt;
     }
 
+   
     public double calculateMonthlyPayment(int months) {
-        if (months <= 0) return 0;
+        if (months <= 0) {
+            return 0;
+        }
         return remainingDebt / months;
     }
 
-    public void pay(double amount) {
+   
+    public boolean pay(double amount) {
         if (amount > 0 && amount <= remainingDebt) {
             remainingDebt -= amount;
+            return true;
         }
+        return false;
+    }
+
+    public boolean isPaidOff() {
+        return remainingDebt == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Loan ID: " + loanId +
+               " | Principal: " + principal +
+               " | Total Debt: " + totalDebt +
+               " | Remaining Debt: " + remainingDebt;
     }
 }
