83e923e13e8ab16e0a084b8b0e8077e3e72fcde5
eylulstudio <eylulbasakdonmez5@gmail.com>
2026-01-01 16:43:07 +0300

    Change Transferable contract to return success status

--- src/com/bank/Account.java
+++ src/com/bank/Account.java
@@ -2,11 +2,11 @@ package com.bank;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public abstract class Account implements Transferable {
 
     protected String accountNumber;
     private double balance;
-    
     protected List<Transaction> transactions;
     protected List<Loan> loans;
 
@@ -17,6 +17,7 @@ public abstract class Account implements Transferable {
         this.loans = new ArrayList<>();
     }
 
+    // Para yatırma
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
@@ -24,27 +25,50 @@ public abstract class Account implements Transferable {
         }
     }
 
+    // Para çekme (polymorphism için abstract)
     public abstract boolean withdraw(double amount);
 
+    // Transfer işlemi
     @Override
-    public void transfer(Account target, double amount) {
+    public boolean transfer(Account target, double amount) {
         if (withdraw(amount)) {
-            transactions.add(new Transaction("Transfer Out", amount));
-
             target.deposit(amount);
+            transactions.add(new Transaction("Transfer Out", amount));
             target.transactions.add(new Transaction("Transfer In", amount));
+            return true;
         }
+        return false;
     }
 
-    public void printTransactions() {
-        for (Transaction t : transactions) {
-            System.out.println(t);
-        }
-    }
-    public void addLoan(Loan loan) {
+    // Kredi alma
+    public void takeLoan(Loan loan) {
         loans.add(loan);
+        deposit(loan.getPrincipal());
+        transactions.add(new Transaction("Loan Taken", loan.getPrincipal()));
     }
 
+    // Kredi ödeme
+    public boolean payLoan(String loanId, double amount) {
+        for (Loan loan : loans) {
+            if (loan.getLoanId().equals(loanId)) {
+
+                if (amount > balance) {
+                    return false;
+                }
+
+                boolean paid = loan.pay(amount);
+                if (paid) {
+                    balance -= amount;
+                    transactions.add(new Transaction("Loan Payment", amount));
+                }
+
+                return paid;
+            }
+        }
+        return false;
+    }
+
+    // Loan listeleme
     public void printLoans() {
         if (loans.isEmpty()) {
             System.out.println("No active loans.");
@@ -52,23 +76,40 @@ public abstract class Account implements Transferable {
         }
 
         for (Loan loan : loans) {
-            System.out.println("Remaining debt: " + loan.getRemainingDebt());
+            System.out.println(loan);
         }
     }
 
-    public double getBalance() {
-        return balance;
+    // Transaction listeleme
+    public void printTransactions() {
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions.");
+            return;
+        }
+
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
     }
 
-    protected void setBalance(double balance) {
-        this.balance = balance;
+    public List<Transaction> getTransactions() {
+        return transactions;
     }
 
     public String getAccountNumber() {
         return accountNumber;
     }
-    public java.util.List<Transaction> getTransactions() {
-        return transactions;
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public List<Loan> getLoans() {
+        return loans;
     }
 
 }
--- src/com/bank/Transferable.java
+++ src/com/bank/Transferable.java
@@ -1,5 +1,5 @@
 package com.bank;
 
 public interface Transferable {
-    void transfer(Account target, double amount);
+    boolean transfer(Account target, double amount);
 }
